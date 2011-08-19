421dfea88108aeb2837f5a72d8458c848d2bf201
spiralofhope <spiralofhope@lavabit.com>
2011-08-19 14:48:35 -0700

    0.0.1_internal

--- push.sh
+++ push.sh
@@ -1,3 +1,8 @@
-git commit -m ''
-git add .
-git push -u origin master
+# This will prompt for a comment.
+# The editor that is summoned can be customized with:
+#   git config --global core.editor <editor>
+# See also $EDITOR
+\git commit
+# git commit -m ''
+\git add .
+\git push -u origin master
