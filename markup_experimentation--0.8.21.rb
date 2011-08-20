48a2233d821cc8264d267e3d354e1a39a87742e9
spiralofhope <spiralofhope@lavabit.com>
2011-08-19 17:59:29 -0700

    markup-experimentation_0.6

--- markup_experimentation--0.8.21.rb
+++ markup_experimentation--0.8.21.rb
@@ -1,3 +1,142 @@
+# http://bfts.rubyforge.org/minitest/
+require 'minitest/autorun'
+
+# http://stackoverflow.com/questions/3772864/how-do-i-remove-leading-whitespace-chars-from-ruby-heredoc/4465640#4465640
+class String
+  # FIXME:  If there is no indenting, this explodes.  Good thing I always indent by at least two.
+
+  # Removes beginning-whitespace from each line of a string.
+  # But only as many whitespace as the first line of text has.
+  #
+  # Meant to be used with heredoc strings like so:
+  #
+  # text = <<-EOS.unindent
+  #   This line has no indentation
+  #     This line has 2 spaces of indentation
+  #   This line is also not indented
+  # EOS
+  #
+  def unindent
+    lines = Array.new
+    each_line { |ln| lines << ln }
+
+    first_line_ws = lines[0].match(/^\s+/)[0]
+    re = Regexp.new('^\s{0,' + first_line_ws.length.to_s + '}')
+
+    lines.collect {|line| line.sub(re, "") }.join
+  end
+end
+
+class Markup_test
+
+  def processable( rx,              # start_rx
+                   end_rx,
+                   process_should ) # string
+    # TODO:  Sanity-checking
+    # Convert the string into an array.
+    process_should = [ process_should ]
+    rx = %r{(#{rx})(.*?)(#{end_rx})}
+    process_should_not = Array.new
+    until process_should[-1].match( rx ) == nil do
+      process_should.delete_at(-1)
+      process_should << $`
+      process_should << nil
+      process_should << $'
+      process_should_not << nil
+      process_should_not << $~[1..3]
+      process_should_not << nil
+    end
+    return process_should, process_should_not
+  end
+#Expected "<u>underlined</u><>_underlined_</>\n"
+     #not "<u>underlined</u><>_underlined_</>\n<u>underlined</u>\n".
+
+  # Array1 replaces its nils with content from array2
+  def combine_arrays( array1, array2 )
+    # TODO:  Sanity-checking
+    array1.each_index do |i|
+      if array1[i] == nil then
+        array1[i] = array2[i]
+      end
+    end
+    return array1
+  end
+
+  def markup( string )
+    process_should, process_should_not = processable( %r{<.*?>}, %r{</.*?>}, string )
+    process_should.each_index do |i|
+      next if process_should[i] == nil
+      # underline
+      if process_should[i].match( %r{(_)(.*?)(_)} ) != nil then
+        process_should[i] = $` + "<u>" + $~[2] + "</u>" + $'
+      end
+    end
+    return combine_arrays( process_should, process_should_not ).join
+  end
+
+end
+
+class Test_Markup_test < MiniTest::Unit::TestCase
+  def setup()
+    @o = Markup_test.new
+    (
+    @document = <<-heredoc.unindent
+        ex.
+        _underlined_
+        <nowiki>
+        _underlined_
+        </nowiki>
+        _underlined_
+    heredoc
+    )
+  end
+
+  def test_u()
+    assert_equal(
+      ( <<-heredoc.unindent
+        <u>underlined</u>
+      heredoc
+      ),
+      @o.markup( <<-heredoc.unindent
+        _underlined_
+      heredoc
+      ),
+    )
+  end
+
+  def test_html_block()
+    assert_equal(
+      ( <<-heredoc.unindent
+        <u>a</u><>_b_</>
+      heredoc
+      ),
+      @o.markup( <<-heredoc.unindent
+        _a_<>_b_</>
+      heredoc
+      ),
+    )
+  end
+
+  def test_u2()
+    assert_equal(
+      ( <<-heredoc.unindent
+        <>_underlined_</>  
+          <u>underlined</u>  
+      heredoc
+      ),
+      @o.markup( <<-heredoc.unindent
+        <>_underlined_</>  
+          _underlined_  
+      heredoc
+      ),
+    )
+  end
+end
+
+
+# --------------------------------
+
+
 class Markup
   def initialize()
   end
@@ -7,7 +146,6 @@ class Markup
     # try to convert it..
     return string
   end
-
   
   def block_processing( start_rx, end_rx, string )
     # TODO: Sanity-checking
@@ -42,7 +180,7 @@ class Markup
     if string.match( rx ) == nil then
       return string
     end
-    html = %r{<.+>.*</.+>}
+    html = %r{<.+?>.*?</.+?>}
     if string.match( html ) == nil then
       string.match( rx )
       string.gsub!(
@@ -64,7 +202,7 @@ class Markup
     # Process the array that's supposed to be processed
     process_should.each_index do |i|
       next if process_should[i] == nil
-      process_should[i] = words_regex( %r{_(.+)_}, "<u>", "</u>", process_should[i] )
+      process_should[i] = words_regex( %r{_(.+?)_}, "<u>", "</u>", process_should[i] )
     end
     
     # Re-combine the two arrays.
@@ -83,37 +221,8 @@ class Markup
   end
 end
 
-# http://stackoverflow.com/questions/3772864/how-do-i-remove-leading-whitespace-chars-from-ruby-heredoc/4465640#4465640
-class String
-  # FIXME:  If there is no indenting, this explodes.  Good thing I always indent by at least two.
-
-  # Removes beginning-whitespace from each line of a string.
-  # But only as many whitespace as the first line of text has.
-  #
-  # Meant to be used with heredoc strings like so:
-  #
-  # text = <<-EOS.unindent
-  #   This line has no indentation
-  #     This line has 2 spaces of indentation
-  #   This line is also not indented
-  # EOS
-  #
-  def unindent
-    lines = Array.new
-    each_line { |ln| lines << ln }
-
-    first_line_ws = lines[0].match(/^\s+/)[0]
-    re = Regexp.new('^\s{0,' + first_line_ws.length.to_s + '}')
-
-    lines.collect {|line| line.sub(re, "") }.join
-  end
-end
-
-# http://bfts.rubyforge.org/minitest/
-require 'minitest/autorun'
-
 # http://bfts.rubyforge.org/minitest/MiniTest/Assertions.html
-class TestMarkup < MiniTest::Unit::TestCase
+class Test_Markup < MiniTest::Unit::TestCase
   def setup()
     @o = Markup.new
     (
