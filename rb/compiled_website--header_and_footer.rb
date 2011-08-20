e3253d1f7a518d0aaac47da203230acab747dccc
spiralofhope <spiralofhope@lavabit.com>
2011-08-19 18:58:21 -0700

    1.2.2_internal

--- rb/compiled_website--header_and_footer.rb
+++ rb/compiled_website--header_and_footer.rb
@@ -1,4 +1,8 @@
 # This needs to be overhauled from all sorts of angles.. but it's "working" right now.
+# TODO:  Make this more universal.  Extract stuff specific to me and make such things into variables.
+
+# used in header_and_footer()
+# require 'pathname'
 
 
 class Markup
