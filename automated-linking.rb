7b4f2b97c481496a3c8818120d2e6b680fd8a709
spiralofhope <spiralofhope@lavabit.com>
2011-08-19 17:37:51 -0700

    automated-linking_0.3.0

--- automated-linking.rb
+++ automated-linking.rb
@@ -1,4 +1,23 @@
-# TODO - totally gut and rework this, to untangle everything.
+=begin
+- TODO: Implement - for unordered lists, and # for ordered lists
+- Consider manual lists.. I count the lines and paint the numbers myself.  This will let me have broken lists like this:
+
+# one
+# two
+<pre>
+something
+</pre>
+# three
+# four
+
+- TODO: Have [http://example.com] type links, which become [1] etc, auto-numbered.
+- TODO: A better 'listings' feature.  Just have it output the right CSS and let the user figure out how it ought to be displayed.  I'm tired of ;list: item
+- TODO: colon (:) for indentation
+- TODO: Manually-indent blocks between headers, like the original coWiki did.  I wonder what the HTML for that was.  Consider looking into my old archives, like my old RPG archives I sent to Angus.
+- TODO: implement "new page" creation concepts.  I would make a link like [[link]] and then the system would point me to the appropriate source .asc file.  This would summon my editor as usual, and I can make the page very easily.
+- TODO: tables, somehow..
+=end
+
 
 =begin
 Requirements:
@@ -13,35 +32,26 @@ HTML Tidy
   make install
 
 
-
-
 Notes:
-I'd love to use Ruby/HTML Tidy, but I don't know how to make it go.
+TODO: I'd love to use Ruby/HTML Tidy, but I don't know how to make it go.
   http://tidy.rubyforge.org/
   http://rubyforge.org/projects/tidy
   http://tidy.rubyforge.org/classes/Tidy.html
   gem install tidy
 =end
 
+=begin
+Usage:
+- You can combine multiple markup by starting it all with spaces between them.
+- wiki-style linking is done automatically
+=end
 
-# TODO - automatic-linking.
-#   I guess I would make some variation on chew.. not sure how I would do that.
-# TODO - I don't think I can remove marked_yes, because that could be useful to make sure I don't have nested markup.
-#   It's ok.  I can just have another markup called _/ and /_  which will combine two (or more) markups.  It'll work out..
-
-
-# working_directory=File.expand_path(File.join('', 'home', 'user', 'live', 'Projects', 'compiled-website', '0.4.7'))
-# lib = File.join(working_directory, 'lib')
-# require File.join(lib, 'misc.rb')
-# require File.join(lib, 'directories.rb')
-# require File.join(lib, 'files.rb')
-# require File.join(lib, 'strings.rb')
 
 def marked_yes(string, search_left, search_right, replace_left, replace_right)
   if string == nil then return '' end
   # TODO: If a link, check that the destination exists
   #  - local = create the file
-  #  - remote = check if it exists, and cache the results?  Only check once a day?
+  #  - remote = check if it exists, and cache the results?  Only check once a day?  Then I can redirect to a page if I know the link is bad, and create a notification to myself.. maybe updating a master status log file.
   if replace_left == nil && replace_right == nil then
     return string
   elsif replace_left == nil then
@@ -57,15 +67,16 @@ def marked_no(string, internal_markup_flag)
   if string == nil then return '' end
   # TODO: Automatic-linking
   # TODO: My own markup
-  if internal_markup_flag == true then
-    internal_markup_flag=true
-  else
-    # TODO: I need to play around more with the regex to handle additional situations.
-    string=chew(string, /(^| )\//, /\/( |.|$)/, '\1<em>', '</em>\1', true)
-    string=chew(string, /(^| )\*\*/, /\*\*( |.|$)/, '\1<big>', '</big>\1', true)
-    string=chew(string, /(^| )\*/, /\*( |.|$)/, '\1<b>', '</b>\1', true)
-    string=chew(string, /(^| )_/, /_( |.|$)/, '\1<u>', '</u>\1', true)
-    string=chew(string, /(^| )-/, /-( |.|$)/, '\1<s>', '</s>\1', true)
+
+  if internal_markup_flag != true then
+    # TODO: If every regex has the same start and end block, I should somehow be able to remove that duplication.. but I'd have to research regexes more and maybe other stuff.  I'm not sure what to do.
+    # TODO: Allow more punctuation on the right side of the search.
+    string=chew(string, /(^| )\//, /\/(\.|$)/, '\1<em>', '</em>\1', true)
+    string=chew(string, /(^| )\*\*/, /\*\*(\.|$)/, '\1<big>', '</big>\1', true)
+    string=chew(string, /(^| )\*/, /\*(\.|$)/, '\1<b>', '</b>\1', true)
+    string=chew(string, /(^| )_/, /_(\.|$)/, '\1<u>', '</u>\1', true)
+    string=chew(string, /(^| )-/, /-(\.|$)/, '\1<s>', '</s>\1', true)
+    string=chew(string, /(^| )((http\:\/\/|ftp:\/\/|irc:\/\/|gopher:\/\/)(.*)(\....?))( |$)/, /( |$)/, '\1<a href="\2">\2</a> ', '\1', true)
     internal_markup_flag=false
   end
   return string
@@ -74,8 +85,9 @@ end
 # TODO: Either do this per-line, or fix the regex and/or \n so that I can make it a whole-file thing.
 def chew(string, search_left, search_right, replace_left, replace_right, internal_markup_flag)
   processed=""
+  # This is to avoid a runaway process:
   count=0
-  until string == nil or count > 20 do
+  until string == nil or count > 100 do
     count+=1
     string.match(/(#{search_left}.*?#{search_right})/)
     if $' != nil then
@@ -93,173 +105,66 @@ def chew(string, search_left, search_right, replace_left, replace_right, interna
   return processed
 end
 
-# document='/example/ text'
-# document='text [[with a link]] text _internal_ /markup/. [[with a link2]] etc.'
-# document=chew(document, /\[\[/, /\]\]/, '<', '>', false)
+# TODO: get the list of files in the current directory, return it as a simple array.. should be trivial to do.
+def get_files()
+  return ['some-example.html', 'some.html']
+end
+# TODO:  Only allow one link for each file?  I'd have to maintain a working array of all the already-created links, and know if I'm creating a duplicate.
+# TODO:  Possibly only one link for every header-section.  Somewhat more complex than the above, but really really worth it.
+def automatic_linking(files, document)
+  document_working=[]
+  document.each do |line|
+    files.each do |file|
+      # 'some-example.html' => 'some example'
+      # dashes become spaces
+      file_string=file.gsub(/-/, ' ')
+      # remove the ending .html
+      file_string=file_string.sub(/\.html$/, '')
 
-document='example [[this _is not_ markup]] string'
-document='*example* [[this _is not_ markup]] string'
-
-document=<<-HEREDOC
-[[basic]]
-[[two words]]
-before [[link here]] after
-[[This _not markup_ example]] string
-
-*bold*
-**big**         (demonstrates presidence)
-_underline_
-/italics/
--strikethrough-
-
-*two words*
-*two _words* test_  - Yes, this is legal in my code.  I'll just clean it up with HTML Tidy.
-HEREDOC
-
-# left=/\[\[/
-# right=//
-#
-# document=chew(document, /\[\[/, /\]\]/, '<', '>', false)
-# puts document
-
-
+      file='./' + file
+      line=chew(line, /(^| )(#{file_string})( |$)/, //, '\1<a href="' + file + '">\2</a>\3', '', true)
+    end
+    document_working << line
+  end
+  return document_working.to_s
+end
 
 document=<<-HEREDOC
+example code:
 *bold* **big** _underline_ /italics/ -strikethrough-
-*two _words* test_
+Yes, this is legal in my code.  I'll just clean it up with HTML Tidy.:
+  *two _words* test_
+< test <> *no markup here* </>
 left <a href="http://example.com">no *markup* here</a> right
-example
+* _bold underline_ * _ *underline bold* _ _ * / underlined bold emphasis / * _
+** * - _ /big bold strikethrough underlined emphasis/_-***
+<nowiki>*test*</nowiki>  <anything>_yep_</> <>don't</>
+left http://example.com right
+http://invalid http://invalid.c
+http://valid.ca
+http://valid.com
+ftp://cool.ca
+*bold*._invalid_
+this is some example text
+this is some text
+this is someexample text
+this issome example text
 HEREDOC
 
-# TODO:  Only allow one link for each file?
-#  Possibly only one link for every header-section.
-#  Possibly only allow x number of links maximum, to avoid issues with linking common things like 'a' or 'the'.
-# files=['some', 'example', 'files']
-# files.each do |file|
-# end
+document=automatic_linking(get_files(), document)
 
-# document=chew(document, /<.*?>/, /<.*?>/, nil, nil, false)
-# Note: This won't work as-expected.  It'll blank out the left and not the right.  This is because my left-search and right-search are the same!
-# document=chew(document, /<.*?>/, /<.*?>/, nil, '', false)
+# Yes it outputs them with spaces between them, but that's ok for HTML. TODO: Check if tidy will clean that up.  Notice the right side doesn't need spaces, and you can even mash *** together.
+document=chew(document, /<.*?>/, /<.*?>/, nil, nil, false)
+
 puts document
 
+# FIXME: tidy isn't installed any more..
 # system('tidy -indent -upper -clean -quiet -omit -asxhtml -access -output file', document)
 
-#  -indent, -i         indent element content
-#  -upper, -u          force tags to upper case
-#  -clean, -c          replace FONT, NOBR and CENTER tags by CSS
-#  -quiet, -q          suppress nonessential output
-#  -omit               omit optional end tags
-#  -asxml, -asxhtml    convert HTML to well formed XHTML
-#  -access <level>     do additional accessibility checks (<level> = 0, 1, 2, 3).
-#                      0 is assumed if <level> is missing.
+
+
 
 
 
 
 __END__
-
-
-
-
-
-
-
-
-
-def automated_linking()
-end
-def test_automated_linking()
-#   working_directory=File.join('', 'tmp', "test_automated_linking.#{$$}")
-#   working_directory=File.expand_path(working_directory)
-#   mcd_directory(working_directory)
-#   create_file(File.join(working_directory, 'file1.html'), '')
-
-  document2=<<-HEREDOC
-[[word1]][[word2]] [[word3]] [[two words]] [[12]] [[wordnum12]] [[]] [[.,-|\/]]
-This is a [[two word]] [[example link]] line.
-This one [[has, punctuation. which ! should? be - ok]]
-This one [[has, punctuation.]]
-A link [[on two
-lines]] isn't possible.
-[[not a link] ]
-[ [not a link]]
-[[an odd ] ] link]]
-  HEREDOC
-
-
-# puts count.inspect, process.inspect
-until count==20 || process=='' do
-  count+=1
-  if process == nil then process="" ; next end
-  process.match(/(\[\[.*?\]\])/)
-
-  if $1 != nil then
-    marked="<MARKUP>#{$`}</MARKUP>"
-    final=final+marked+$1
-    process=$'
-    next
-  else
-    process=$'
-  end
-
-
-# puts process.inspect
-end
-puts document
-puts '----'
-puts final
-
-#   return $` + replacement + $'
-
-
-
-# stop=false
-# until stop=true do
-# end
-
-
-
-  array[left..right].each do |i|
-
-document.each do |line|
-  line=line.split(/\b/)
-  left=0
-  right=line.length
-  until left==right do
-    test2(line, left, right)
-  end
-end
-
-
-# puts document.inspect
-
-files.each do |file|
-  line=""
-  position=0
-  document.split(/\b/).each do |word|
-    position=position+1
-    puts word.inspect
-    if word == "\n" then line="" end
-    line=line+word
-    if file == line then
-      puts "matched"
-      line=""
-    end
-#     puts f, word
-  end
-end
-
-
-
-
-  if ( match =~ string ) != nil then
-    return $` + replacement + $'
-  else
-
-
-
-# Regular expressions..
-(\[{2}[a-z \.,]*\]{2})
-(\[{2}[\w\s\d.,|()_\/\-\\]+\]{2})
-(\[\[.*?\]\])
